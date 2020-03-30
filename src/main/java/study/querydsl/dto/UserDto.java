3ccf5267dbe18ec08a4d9c90fe5efe62c8f430a5
joohyuk <vfrvfr4207@gmail.com>
2020-03-30 20:09:15 +0900

    @QueryProjection

---
+++ src/main/java/study/querydsl/dto/UserDto.java
@@ -0,0 +1,12 @@
+package study.querydsl.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class UserDto {
+
+    private String name;
+    private int age;
+}
